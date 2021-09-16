e978c8ed579552d980520db66a3e969553b10e43
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-09-16 16:04:20 -0500

    done

---
+++ WPF-Classes/WPF-Classes/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Addbtb_Click(object sender, RoutedEventArgs e)
+        {
+
+          
+            string name, manufacturer, image, price;
+
+            double p = 0;
+            name = txtN.Text;
+            manufacturer = txtM.Text;
+            image = Imagetxt.Text;
+            price = txtP.Text;
+
+            if (string.IsNullOrEmpty(name)==true)
+            {
+                MessageBox.Show("invalid name" );
+            }
+            if (string.IsNullOrEmpty(manufacturer)==true)
+            {
+                MessageBox.Show("imvalid manufacturer");
+            }
+            if (string.IsNullOrEmpty(image)==true)
+            {
+                MessageBox.Show("invalid image");
+            }
+            if (double.TryParse(price, out p)==false) 
+            {
+                MessageBox.Show("invalid price");
+            }
+
+            Toy ListToy = new Toy()
+            {
+                Manufacturer = manufacturer, 
+                Name= name, 
+                image=image,
+                price=p, 
+            };
+            TT.Items.Add(ListToy);
+            }
+
+
+        private void ListToy_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Toy selectedToy = (Toy)TT.SelectedItem;
+            var uri = new Uri(selectedToy.image);
+            var img = new BitmapImage(uri);
+            imgbox.Source = img;
+            MessageBox.Show($"{selectedToy.GetAisle()}");
+        }
+    }
+
+}
---
+++ WPF-Classes/WPF-Classes/Toy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Classes
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double price { get; set; }
+        public string image { get; set; }
+        private string Aisle;
+
+        public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            price = 0;
+            image = "";
+            Aisle = "";
+
+        }
+        public string GetAisle()
+        {
+            return $"{Manufacturer[0].ToString().ToUpper()}{price}";
+
+
+
+
+
+
+        }
+        public override string ToString()
+        {
+            return $"{ Manufacturer} -{ Name}";
+
+
+        }
+    }
+    }
+
